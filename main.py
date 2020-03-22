4d040f7ef0f779ec68c8b8c97dc05e7d429aae63
WenHanGao <gaowenhan13@gmail.com>
2020-03-23 00:12:20 +0800

    update

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ Created on Wed Feb 12 12:26:24 2020
 
 from DocumentClass import Document
 
-doc = Document('example10.xopp')
+doc = Document('example1.xopp')
 doc.prettyPrint()
 #doc.evaluatePage(0, 'Solve')
 #doc.evaluatePage(0, 'Reduce')
 
-doc.toLatex()
+#doc.toLatex()
