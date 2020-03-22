84a1af9347d1bd1ad62d65f6b71309468f2a97ef
WenHanGao <gaowenhan13@gmail.com>
2020-03-23 00:10:34 +0800

    update

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ Created on Wed Feb 12 12:26:24 2020
 
 from DocumentClass import Document
 
-doc = Document('example1.xopp')
+doc = Document('example10.xopp')
 doc.prettyPrint()
-doc.evaluatePage(0, 'Solve')
-doc.evaluatePage(1, 'Reduce')
-doc.evaluatePage(2, 'Solve')
+#doc.evaluatePage(0, 'Solve')
+#doc.evaluatePage(0, 'Reduce')
 
 doc.toLatex()
